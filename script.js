e93dde66e9de2a86dfd0f75f227de27e8efa8df4
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2022-04-25 12:54:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+var pontos = 0 
+let p1 = document.getElementById('p1')
+let p2 = document.getElementById('p2')
+let p3 = document.getElementById('p3')
+let p4 = document.getElementById('p4')
+let p5 = document.getElementById('p5')
+let p6 = document.getElementById('p6')
+let p7 = document.getElementById('p7')
+let p8 = document.getElementById('p8')
+
+let botao = document.querySelector('#pontuar')
+botao.addEventListener('click', pontuar)
+
+function pontuar() {
+    if (('p1').checked) 
+    {
+        pontos++
+    }
+    if (p2.value == "2")
+    {
+        pontos++
+    }
+    if (p3.value == "2")
+    {
+        pontos++
+    }
+    if (p4.value == "2")
+    {
+        pontos++
+    }
+    if (p5.value == "2")
+    {
+        pontos++
+    }
+    if (p6.value == "2")
+    {
+        pontos++
+    }
+    if (p7.value == "2")
+    {
+        pontos++
+    }
+    if (p8.value == "2")
+    {
+        pontos++
+    }
+    console.log(pontos);
+}
