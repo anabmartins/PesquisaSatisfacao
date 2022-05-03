bf2f09241aeec7bd276027e12d1d4f2e937963b6
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2022-05-03 16:46:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,48 @@
 var pontos = 0 
-let p1 = document.getElementByName('p1')
-let p2 = document.getElementById('p2')
-let p3 = document.getElementById('p3')
-let p4 = document.getElementById('p4')
-let p5 = document.getElementById('p5')
-let p6 = document.getElementById('p6')
-let p7 = document.getElementById('p7')
-let p8 = document.getElementById('p8')
+let p1 = document.getElementsByName("p1")
+let p2 = document.getElementsByName("p2")
+let p3 = document.getElementsByName("p3")
+let p4 = document.getElementsByName("p4")
+let p5 = document.getElementsByName("p5")
+let p6 = document.getElementsByName("p6")
+let p7 = document.getElementsByName("p7")
+let p8 = document.getElementsByName("p8")
+
+
 
 let botao = document.querySelector('#pontuar')
 botao.addEventListener('click', pontuar)
 
 function pontuar() {
-    if (("#op10").checked) 
+    if (("#1.10").checked) 
     {
         pontos++
     }
-    if (p2.value == "2")
+    if (("#2.2").checked)
     {
         pontos++
     }
-    if (p3.value == "2")
+    if (("#3.1").checked)
     {
         pontos++
     }
-    if (p4.value == "2")
+    if (("#4.1").checked)
     {
         pontos++
     }
-    if (p5.value == "2")
+    if (("#5.3").checked)
     {
         pontos++
     }
-    if (p6.value == "2")
+    if (("#6.1").checked)
     {
         pontos++
     }
-    if (p7.value == "2")
+    if (("#7.1").checked)
     {
         pontos++
     }
-    if (p8.value == "2")
+    if (("#8.1").checked)
     {
         pontos++
     }
