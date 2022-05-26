d30150eb2352b0fd9724312543f7515892c07356
Ana Beatriz Martins <99266498+anabmartins@users.noreply.github.com>
2022-05-26 20:52:56 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,50 +1,38 @@
 var pontos = 0 
-let p1 = document.getElementsByName("p1")
-let p2 = document.getElementsByName("p2")
-let p3 = document.getElementsByName("p3")
-let p4 = document.getElementsByName("p4")
-let p5 = document.getElementsByName("p5")
-let p6 = document.getElementsByName("p6")
-let p7 = document.getElementsByName("p7")
-let p8 = document.getElementsByName("p8")
-
-
-
-let botao = document.querySelector('#pontuar')
-botao.addEventListener('click', pontuar)
+var p1 = document.getElementsByName("p1")
+var p2 = document.getElementsByName("p2")
+var p3 = document.getElementsByName("p3")
+var p4 = document.getElementsByName("p4")
+var p5 = document.getElementsByName("p5")
+var p6 = document.getElementsByName("p6")
+var p7 = document.getElementsByName("p7")
+var p8 = document.getElementsByName("p8")
 
 function pontuar() {
-    if (("#1.10").checked) 
-    {
-        pontos++
-    }
-    if (("#2.2").checked)
-    {
-        pontos++
-    }
-    if (("#3.1").checked)
-    {
-        pontos++
-    }
-    if (("#4.1").checked)
-    {
-        pontos++
-    }
-    if (("#5.3").checked)
-    {
-        pontos++
-    }
-    if (("#6.1").checked)
-    {
-        pontos++
-    }
-    if (("#7.1").checked)
-    {
-        pontos++
-    }
-    if (("#8.1").checked)
-    {
-        pontos++
-    }
-    console.log(pontos);
+if (p1[4].checked){
+pontos++
+} 
+if(p2[0].checked || p2[2].checked || p2[3].checked){
+pontos++
+} 
+if(p3[0].checked){
+pontos++
+}
+if(p4[0].checked){
+pontos++
+}
+if(p5.checked){
+pontos++
+}
+if(p6[0].checked || p6[1].checked){
+pontos++
+}
+if(p7[0].checked){
+pontos++
+}
+if(p8[0].checked || p8[1].checked){
+pontos++
+}
+resultado.innerHTML = String(pontos)
+pontos = 0
 }
